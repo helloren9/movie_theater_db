58703248bc1a1d6d6d6476e5659ad876ea27793f
Loren Anunciacion <loren.anunciacion@gmail.com>
2024-03-12 00:58:06 -0700

    Add files via upload

--- theater_create_table.sql
+++ theater_create_table.sql
@@ -1,25 +1,25 @@
-CREATE TABLE customers (
-	customer_id SERIAL PRIMARY KEY,
+create table customers (
+	customer_id SERIAL primary key,
 	name VARCHAR(100),
 	email VARCHAR(100)
 );
 
-CREATE TABLE movies (
-	movie_id SERIAL PRIMARY KEY,
+create table movies (
+	movie_id SERIAL primary key,
 	title VARCHAR(100),
 	genre VARCHAR(100)
 );
 
-CREATE TABLE tickets (
-	ticket_id SERIAL PRIMARY KEY,
-	customer_id INT REFERENCES customer(customer_id),
-	movie_id INT REFERENCES movies(movie_id),
+create table tickets (
+	ticket_id SERIAL primary key,
+	foreign key customer_id INT references customer(customer_id),
+	foreign key movie_id INT references movies(movie_id),
 	seat_number VARCHAR(10)
 );
 
-CREATE TABLE concessions (
-	concession_id SERIAL PRIMARY KEY,
-	movie_id INT REFERENCES movies(movie_id),
+create table concessions (
+	concession_id SERIAL primary key,
+	foreign key movie_id INT references movies(movie_id),
 	item_name VARCHAR(100),
 	price DECIMAL(10, 2)
 );
